c614cae50eafddcde9beffd7c7f7f65b45ae4945
Marc <marcllopis89@gmail.com>
2022-04-11 15:37:10 +0200

    weather

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const apiUrlBarcelona =
+  "https://api.openweathermap.org/data/2.5/weather?q=Barcelona&units=metric&appid=4c28746bdba2dff0e79219c825e7039a";
+const apiUrlBerlin =
+  "https://api.openweathermap.org/data/2.5/weather?q=Berlin&units=metric&appid=4c28746bdba2dff0e79219c825e7039a";
 
 function App() {
+  let [weather, setWeather] = useState({});
+  let [apiLoaded, setApiLoaded] = useState(false);
+
+  useEffect(() => {
+    // call the api
+    // wait until we get the api call coming back with smth
+    // store the api result in our state
+
+    fetch(apiUrlBarcelona)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeather(data);
+        setApiLoaded(true);
+      });
+  }, []);
+
+  const handleApiCall = () => {
+    fetch(apiUrlBerlin)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeather(data);
+        setApiLoaded(true);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My weather app</h1>
+      {apiLoaded ? (
+        <>
+          <h2>Weather in Barcelona</h2>
+          <p>Temperature: {weather.main.temp}</p>
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+            alt={weather.weather[0].description}
+          />
+          <p>Status: {weather.weather[0].description}</p>
+        </>
+      ) : (
+        <h2>Loading weather...</h2>
+      )}
+      <p>
+        Do you want to know the weather in Berlin?{" "}
+        <button onClick={handleApiCall}>Click here!</button>
+      </p>
     </div>
   );
 }
